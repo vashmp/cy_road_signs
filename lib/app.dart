5efcff4943fcb0ff659107835a9044e9b9080e25
Nikita Blokhin <v45hjj@gmail.com>
2025-03-12 18:24:44 +0200

    feat: add sign details screen and navigation, added hero animation between screens

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:cy_road_signs/screens/signs/sign_details.dart';
 import 'package:flutter/material.dart';
 import 'screens/home/home_screen.dart';
 
@@ -6,6 +7,13 @@ class Application extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      routes: {
+        '/signs/details': (context) => const DetailsScreen(
+              tag: '',
+              image: '',
+              name: '',
+            ),
+      },
       title: 'Cyprus Road Signs',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -13,6 +21,5 @@ class Application extends StatelessWidget {
       ),
       home: HomeScreen(),
     );
-    //   );
   }
 }
---
+++ lib/screens/signs/sign_details.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class DetailsScreen extends StatelessWidget {
+  const DetailsScreen(
+      {Key? key, required this.tag, required this.image, required this.name})
+      : super(key: key);
+  final String tag;
+  final String image;
+  final String name;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Warning Signs'),
+      ),
+      body: Hero(
+        tag: tag,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset(
+                image!,
+                fit: BoxFit.contain,
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                name!,
+                style: TextStyle(fontSize: 24),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
