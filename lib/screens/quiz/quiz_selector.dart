8c6f7ddaf04d921d7c62f5063daf138ecbc60598
Nikita Blokhin <v45hjj@gmail.com>
2025-03-13 17:37:58 +0200

    created reversed test for 1 text and 4 images, splited quiz selector screen for two columns

--- lib/screens/quiz/quiz_selector.dart
+++ lib/screens/quiz/quiz_selector.dart
@@ -1,5 +1,6 @@
+import 'package:cy_road_signs/screens/quiz/image_quiz_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:cy_road_signs/screens/quiz/quiz_screen.dart';
+import 'package:cy_road_signs/screens/quiz/text_quiz_screen.dart';
 import 'package:cy_road_signs/widgets/common_widgets.dart';
 
 class SignQuizSelector extends StatelessWidget {
@@ -16,22 +17,75 @@ class SignQuizSelector extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text('Text', style: Theme.of(context).textTheme.titleLarge),
+                const SizedBox(width: 20),
+                Text('Image', style: Theme.of(context).textTheme.titleLarge),
+              ],
+            ), // Row 1 Header text
             const SizedBox(height: 20),
-            actionButton('10 Questions', () {
-              navigateToScreen(context, QuizScreen(numberOfQuestions: 10));
-            }),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                actionButton('10 Questions', () {
+                  navigateToScreen(
+                      context, TextQuizScreen(numberOfQuestions: 10));
+                }),
+                const SizedBox(width: 20),
+                actionButton('10 Questions', () {
+                  navigateToScreen(
+                      context, ImageQuizScreen(numberOfQuestions: 10));
+                }),
+              ],
+            ), // Row 2
             const SizedBox(height: 20),
-            actionButton('20 Questions', () {
-              navigateToScreen(context, QuizScreen(numberOfQuestions: 20));
-            }),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                actionButton('30 Questions', () {
+                  navigateToScreen(
+                      context, TextQuizScreen(numberOfQuestions: 30));
+                }),
+                const SizedBox(width: 20),
+                actionButton('30 Questions', () {
+                  navigateToScreen(
+                      context, ImageQuizScreen(numberOfQuestions: 30));
+                }),
+              ],
+            ), // Row 3
             const SizedBox(height: 20),
-            actionButton('50 Questions', () {
-              navigateToScreen(context, QuizScreen(numberOfQuestions: 50));
-            }),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                actionButton('50 Questions', () {
+                  navigateToScreen(
+                      context, TextQuizScreen(numberOfQuestions: 50));
+                }),
+                const SizedBox(width: 20),
+                actionButton('50 Questions', () {
+                  navigateToScreen(
+                      context, ImageQuizScreen(numberOfQuestions: 50));
+                }),
+              ],
+            ), // Row 4
             const SizedBox(height: 20),
-            actionButton('All Questions', () {
-              navigateToScreen(context, QuizScreen(numberOfQuestions: -1));
-            }),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                actionButton('All Questions', () {
+                  navigateToScreen(
+                      context, TextQuizScreen(numberOfQuestions: -1));
+                }),
+                const SizedBox(width: 20),
+                actionButton('All Questions', () {
+                  navigateToScreen(
+                      context, ImageQuizScreen(numberOfQuestions: -1));
+                }),
+              ],
+            ), // Row 5
+            const SizedBox(height: 20),
           ],
         ),
       ),
